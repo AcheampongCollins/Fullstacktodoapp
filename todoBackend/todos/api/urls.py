6c999d7d29c234b6f2320af254490da37d402362
Collins Owusu <owusucollins51@gmail.com>
2022-09-12 11:02:32 +0000

    simplejwt configuration

--- todoBackend/todos/api/urls.py
+++ todoBackend/todos/api/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
+from .views import  MyTokenObtainPairView
 from . import views
 
 
-from rest_framework_simplejwt.views import (
 
-    TokenObtainPairView,
+from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
 
 urlpatterns = [
      path('', views.getRoute),
-     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+     path('token/',  MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
      path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
